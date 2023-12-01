eafc867fc2dd8b8f1c6b45e27d1ae3e6c2bf8b78
Marcus Bolmstedt <marcus@bolmstedt.com>
2023-12-01 20:26:38 +0100

    feat: add timeout and error handling

--- python/aoc/solver.py
+++ python/aoc/solver.py
@@ -7,6 +7,10 @@ from rich import progress
 from aoc import solution
 
 
+class SolutionError(Exception):
+    """Raised when a solution is broken."""
+
+
 class SolutionResult(object):
     def __init__(self, solution_result: int | str):
         self._solution_result = solution_result
@@ -38,28 +42,43 @@ class Solver(object):
         solution_result = SolutionResult(self._solution.solve_part_two())
         typer.echo("- Part Two: {result}".format(result=solution_result))
 
-    def benchmark(self, iterations: int = 1) -> None:
+    def benchmark(self, iterations: int = 1, timeout: float = 0.1) -> None:
         typer.echo("Benchmarking {name}".format(name=self._solution.solution_name))
-        solution_time = _benchmark(
-            name="- Part One:",
-            func=self._solution.solve_part_one,
-            iterations=iterations,
-        )
-        typer.secho(solution_time, fg=_get_color(solution_time))
-        solution_time = _benchmark(
-            name="- Part Two:",
-            func=self._solution.solve_part_two,
-            iterations=iterations,
-        )
-        typer.secho(solution_time, fg=_get_color(solution_time))
+
+        try:
+            solution_time = _benchmark(
+                name="- Part One:",
+                func=self._solution.solve_part_one,
+                iterations=iterations,
+                timeout=timeout,
+            )
+        except SolutionError as err:
+            typer.secho(str(err), fg=typer.colors.RED)
+        else:
+            typer.secho(solution_time, fg=_get_color(solution_time))
+
+        try:
+            solution_time = _benchmark(
+                name="- Part Two:",
+                func=self._solution.solve_part_two,
+                iterations=iterations,
+                timeout=timeout,
+            )
+        except SolutionError as err:
+            typer.secho(str(err), fg=typer.colors.RED)
+        else:
+            typer.secho(solution_time, fg=_get_color(solution_time))
 
 
 def _benchmark(
     name: str,
     func: Callable[[], int | str],
     iterations: int = 1,
+    timeout: float = 0.2,
 ) -> str:
+    max_total_time = iterations * timeout * 1000000000
     total = 0
+    iteration = 0
     progress_bar = progress.track(
         sequence=range(iterations),
         description=name,
@@ -68,12 +87,20 @@ def _benchmark(
     )
 
     for _ in progress_bar:
+        iteration += 1
         start = time.perf_counter_ns()
-        func()
+        try:
+            func()
+        except Exception as err:  # noqa: BLE001
+            raise SolutionError("Solution is broken") from err
+
         end = time.perf_counter_ns()
         total += end - start
 
-    average = _get_human_readable_time(total / iterations)
+        if total > max_total_time:
+            break
+
+    average = _get_human_readable_time(total / iteration)
 
     return "{name} {time}".format(name=name, time=average)
 
