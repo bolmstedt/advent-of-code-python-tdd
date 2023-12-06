ad65c113a7bf96167dad6a13f0373b1f6c6267fe
Marcus Bolmstedt <marcus@bolmstedt.com>
2023-12-06 23:11:33 +0100

    feat(2023:05): add part 2 solution

--- python/aoc/year_2023/day_05_if_you_give_a_seed_a_fertilizer/solution.py
+++ python/aoc/year_2023/day_05_if_you_give_a_seed_a_fertilizer/solution.py
@@ -1,6 +1,19 @@
 import copy
+from typing import NamedTuple
+
+import more_itertools
 
 from aoc import solution
+
+
+class Mapping(NamedTuple):
+    source: range
+    shift: int
+
+
+class Map(NamedTuple):
+    name: str
+    mappings: list[Mapping]
 
 
 class Solution(solution.Solution):
@@ -12,10 +25,27 @@ class Solution(solution.Solution):
         return min(self._apply_soil_mappings(self._get_soils()))
 
     def solve_part_two(self) -> int:
-        return -1
+        mapped = self._apply_range_mappings(self._get_soil_ranges())
+
+        if not mapped:
+            return -1
+
+        return min(soil.start for soil in mapped if soil.start > 0)
 
     def _get_soils(self) -> set[int]:
         return set(map(int, self._input_lines[0][7:].split(" ")))
+
+    def _get_soil_ranges(self) -> list[range]:
+        soils = [
+            range(soil, soil + length)
+            for soil, length in more_itertools.chunked(
+                map(int, self._input_lines[0][7:].split(" ")),
+                2,
+            )
+        ]
+        soils.sort(key=lambda x: x.start)
+
+        return soils
 
     def _apply_soil_mappings(self, soils: set[int]) -> set[int]:
         current_soils = copy.copy(soils)
@@ -43,3 +73,86 @@ class Solution(solution.Solution):
             current_soils = mapped_soils.union(unmapped_soils)
 
         return current_soils
+
+    def _apply_range_mappings(self, soils: list[range]) -> list[range]:
+        for soil_map in self._create_maps():
+            soils = apply_range_map(soils, soil_map)
+
+        return soils
+
+    def _create_maps(self) -> list[Map]:
+        maps = []
+        map_name = ""
+        mappings: list[Mapping] = []
+
+        for line in self._input_lines[1:]:
+            if ":" in line:
+                map_name = line[:-1]
+                continue
+
+            if not line:
+                if mappings:
+                    mappings.sort(key=lambda x: x.source.start)
+                    maps.append(
+                        Map(
+                            name=map_name,
+                            mappings=mappings,
+                        ),
+                    )
+                    mappings = []
+
+                continue
+
+            destination, source, length = map(int, line.split(" "))
+            mappings.append(
+                Mapping(
+                    source=range(source, source + length),
+                    shift=destination - source,
+                ),
+            )
+
+        if mappings:
+            mappings.sort(key=lambda x: x.source.start)
+            maps.append(
+                Map(
+                    name=map_name,
+                    mappings=mappings,
+                ),
+            )
+
+        return maps
+
+
+def apply_range_map(soils: list[range], soil_map: Map) -> list[range]:
+    soils_to_shift = []
+
+    for mapping in soil_map.mappings:
+        for index, soil in enumerate(soils):
+            overlap = range_overlap(soil, mapping.source)
+
+            if overlap:
+                soils.pop(index)
+                if overlap.start > soil.start:
+                    soils.append(range(soil.start, overlap.start))
+
+                soils_to_shift.append(overlap)
+
+                if overlap.stop < soil.stop:
+                    soils.append(range(overlap.stop, soil.stop))
+
+    soils.extend(
+        _shift_range(soil, mapping.shift)
+        for mapping in soil_map.mappings
+        for soil in soils_to_shift
+        if range_overlap(soil, mapping.source)
+    )
+
+    return soils
+
+
+def _shift_range(range_: range, shift: int) -> range:
+    return range(range_.start + shift, range_.stop + shift)
+
+
+def range_overlap(first: range, second: range) -> range:
+    return range(max(first.start, second.start), min(first.stop, second.stop))
--- python/aoc/year_2023/day_05_if_you_give_a_seed_a_fertilizer/solution_test.py
+++ python/aoc/year_2023/day_05_if_you_give_a_seed_a_fertilizer/solution_test.py
@@ -17,3 +17,40 @@ from aoc.year_2023.day_05_if_you_give_a_seed_a_fertilizer import solution
 )
 def test_part_one(input_data: str, expected: int) -> None:
     assert solution.Solution(input_data).solve_part_one() == expected
+
+
+@pytest.mark.parametrize(
+    ("input_data", "expected"),
+    [
+        ("seeds: 1 2", 1),
+        ("seeds: 1 3\nmap:\n3 1 1", 2),
+        (loader.get_module_data(solution, "fixture.txt"), 46),
+        (loader.get_input_data(solution), 2520479),
+    ],
+)
+def test_part_two(input_data: str, expected: int) -> None:
+    assert solution.Solution(input_data).solve_part_two() == expected
+
+
+@pytest.mark.parametrize(
+    ("soils", "soil_map", "expected"),
+    [
+        (
+            [range(1, 10)],
+            solution.Map(
+                name="foo",
+                mappings=[
+                    solution.Mapping(source=range(5, 6), shift=10),
+                    solution.Mapping(source=range(8, 9), shift=10),
+                ],
+            ),
+            [range(1, 5), range(6, 8), range(9, 10), range(15, 16), range(18, 19)],
+        ),
+    ],
+)
+def test_range_application(
+    soils: list[range],
+    soil_map: solution.Map,
+    expected: list[range],
+) -> None:
+    assert solution.apply_range_map(soils, soil_map) == expected
